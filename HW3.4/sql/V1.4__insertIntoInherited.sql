6fa2cfe462c9f84d4a195d02245e90b66cc2bed0
Edward <op_230@mail.ru>
2020-04-02 19:12:00 +0300

    insert script fix

--- HW3.4/sql/V1.4__insertIntoInherited.sql
+++ HW3.4/sql/V1.4__insertIntoInherited.sql
@@ -1,2 +1,3 @@
 INSERT INTO inherited_hub(id, name)
-SELECT i, 'test'::varchar FROM generate_series(1, 1000000) AS k(i);
+    SELECT (random() * 100000::integer), 'test'
+        FROM generate_series(1, 1000000) AS k(i);
