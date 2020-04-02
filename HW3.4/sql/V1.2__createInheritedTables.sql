755ff4f77402e35e01066376d357c724d623a8f4
Edward <op_230@mail.ru>
2020-04-02 16:45:00 +0300

    Inherited table constarints fix

--- HW3.4/sql/V1.2__createInheritedTables.sql
+++ HW3.4/sql/V1.2__createInheritedTables.sql
@@ -16,21 +16,21 @@ CREATE TABLE inherited10() INHERITS (inherited_hub);
 
 ALTER TABLE inherited1 ADD CONSTRAINT partition_check
 CHECK (id > 0 and id <= 100000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited2 ADD CONSTRAINT partition_check
 CHECK (id > 100000 and id <= 200000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited3 ADD CONSTRAINT partition_check
 CHECK (id > 200000 and id <= 300000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited4 ADD CONSTRAINT partition_check
 CHECK (id > 300000 and id <= 400000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited5 ADD CONSTRAINT partition_check
 CHECK (id > 400000 and id <= 500000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited6 ADD CONSTRAINT partition_check
 CHECK (id > 500000 and id <= 600000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited7 ADD CONSTRAINT partition_check
 CHECK (id > 600000 and id <= 100000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited8 ADD CONSTRAINT partition_check
 CHECK (id > 700000 and id <= 100000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited9 ADD CONSTRAINT partition_check
 CHECK (id > 800000 and id <= 100000);
-ALTER TABLE inherited1 ADD CONSTRAINT partition_check
+ALTER TABLE inherited10 ADD CONSTRAINT partition_check
 CHECK (id > 900000 and id <= 1000000);
