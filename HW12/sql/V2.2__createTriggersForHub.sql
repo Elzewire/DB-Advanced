2d4a72a2f11fb43d54d2e684730e6e3f54ba8530
Edward <op_230@mail.ru>
2020-04-19 21:21:35 +0300

    HW12 update

--- HW12/sql/V2.2__createTriggersForHub.sql
+++ HW12/sql/V2.2__createTriggersForHub.sql
@@ -2,14 +2,14 @@ CREATE OR REPLACE FUNCTION redirect_updates()
     RETURNS TRIGGER AS
     $BODY$
     DECLARE
-        old_child_id integer := DIV(OLD.id, 100000) + 1;
+        old_child_id integer := DIV(OLD.id, 100000);
         old_child_name varchar := 'inh_trg_child' || old_child_id;
-        new_child_id integer := DIV(NEW.id, 100000) + 1;
+        new_child_id integer := DIV(NEW.id, 100000);
         new_child_name varchar := 'inh_trg_child' || new_child_id;
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = new_child_name) THEN
             EXECUTE format('CREATE TABLE %s() INHERITS (%s)', new_child_name, tg_table_name);
-            EXECUTE format('ALTER TABLE %s ADD CONSTRAINT partition_check CHECK (id > (%s - 1) * 100000 and id <= %s * 100000)', new_child_name, new_child_id, new_child_id);
+            EXECUTE format('ALTER TABLE %s ADD CONSTRAINT partition_check CHECK (id >= %s * 100000 and id < (%s + 1) * 100000)', new_child_name, new_child_id, new_child_id);
             EXECUTE format('CREATE TRIGGER trg_redirect_updates ' ||
                            'BEFORE UPDATE OF id ON %s FOR EACH ROW ' ||
                            'WHEN (OLD.id IS DISTINCT FROM NEW.id)' ||
@@ -26,12 +26,12 @@ CREATE OR REPLACE FUNCTION redirect_inserts()
     RETURNS TRIGGER AS
     $BODY$
     DECLARE
-        child_id integer := DIV(NEW.id, 100000) + 1;
+        child_id integer := DIV(NEW.id, 100000);
         child_name varchar := 'inh_trg_child' || child_id;
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = child_name) THEN
             EXECUTE format('CREATE TABLE %s() INHERITS (%s)', child_name, tg_table_name);
-            EXECUTE format('ALTER TABLE %s ADD CONSTRAINT partition_check CHECK (id > (%s - 1) * 100000 and id <= %s * 100000)', child_name, child_id, child_id);
+            EXECUTE format('ALTER TABLE %s ADD CONSTRAINT partition_check CHECK (id >= %s * 100000 and id < (%s + 1) * 100000)', child_name, child_id, child_id);
             EXECUTE format('CREATE TRIGGER trg_redirect_updates ' ||
                            'BEFORE UPDATE OF id ON %s FOR EACH ROW ' ||
                            'WHEN (OLD.id IS DISTINCT FROM NEW.id)' ||
