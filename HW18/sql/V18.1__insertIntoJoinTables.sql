061dacaabe623cc83db88340ae083a942727402e
Edward <op_230@mail.ru>
2020-04-27 17:26:41 +0300

    HW18 added

--- HW18/sql/V18.1__insertIntoJoinTables.sql
+++ HW18/sql/V18.1__insertIntoJoinTables.sql
@@ -3,29 +3,29 @@ INSERT INTO t1(id, name)
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t2(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t3(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t4(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t5(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t6(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t7(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
 
 INSERT INTO t8(id, name)
-    SELECT (random() * 100000)::integer, 'test' || i
+    SELECT (random() * 10000)::integer, 'test' || i
         FROM generate_series(1, 10000) AS g(i);
