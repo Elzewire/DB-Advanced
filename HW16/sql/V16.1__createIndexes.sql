20979c551a99ed7deb244f75d3df1a9042435c7f
Edward <op_230@mail.ru>
2020-04-27 14:53:40 +0300

    HW16 added

--- HW16/sql/V16.1__createIndexes.sql
+++ HW16/sql/V16.1__createIndexes.sql
@@ -1,24 +1,24 @@
 /* Single element indexes */
-CREATE INDEX public.a_idx ON public.b_tree_index_table(a);
-CREATE INDEX public.b_idx ON public.b_tree_index_table(b);
-CREATE INDEX public.c_idx ON public.b_tree_index_table(c);
+CREATE INDEX a_idx ON public.b_tree_index_table(a);
+CREATE INDEX b_idx ON public.b_tree_index_table(b);
+CREATE INDEX c_idx ON public.b_tree_index_table(c);
 
 /* Two element indexes */
-CREATE INDEX public.ab_idx ON public.b_tree_index_table(a, b);
-CREATE INDEX public.ac_idx ON public.b_tree_index_table(a, c);
+CREATE INDEX ab_idx ON public.b_tree_index_table(a, b);
+CREATE INDEX ac_idx ON public.b_tree_index_table(a, c);
 
-CREATE INDEX public.ba_idx ON public.b_tree_index_table(b, a);
-CREATE INDEX public.bc_idx ON public.b_tree_index_table(b, c);
+CREATE INDEX ba_idx ON public.b_tree_index_table(b, a);
+CREATE INDEX bc_idx ON public.b_tree_index_table(b, c);
 
-CREATE INDEX public.ca_idx ON public.b_tree_index_table(c, a);
-CREATE INDEX public.cb_idx ON public.b_tree_index_table(c, b);
+CREATE INDEX ca_idx ON public.b_tree_index_table(c, a);
+CREATE INDEX cb_idx ON public.b_tree_index_table(c, b);
 
 /* Three element indexes */
-CREATE INDEX public.abc_idx ON public.b_tree_index_table(a, b, c);
-CREATE INDEX public.acb_idx ON public.b_tree_index_table(a, c, b);
+CREATE INDEX abc_idx ON public.b_tree_index_table(a, b, c);
+CREATE INDEX acb_idx ON public.b_tree_index_table(a, c, b);
 
-CREATE INDEX public.bac_idx ON public.b_tree_index_table(b, a, c);
-CREATE INDEX public.bca_idx ON public.b_tree_index_table(b, c, a);
+CREATE INDEX bac_idx ON public.b_tree_index_table(b, a, c);
+CREATE INDEX bca_idx ON public.b_tree_index_table(b, c, a);
 
-CREATE INDEX public.cab_idx ON public.b_tree_index_table(c, a, b);
-CREATE INDEX public.cba_idx ON public.b_tree_index_table(c, b, a);
+CREATE INDEX cab_idx ON public.b_tree_index_table(c, a, b);
+CREATE INDEX cba_idx ON public.b_tree_index_table(c, b, a);
