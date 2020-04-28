226231736b018f51dee270269d3bf075a5b39a80
Edward <op_230@mail.ru>
2020-04-28 13:31:31 +0300

    Added HW10

---
+++ HW10/bench_scripts/select_first.sql
@@ -0,0 +1,3 @@
+BEGIN;
+    SELECT sum(b100) FROM aligned_table2;
+END;
---
+++ HW10/sql/V22.1__insertMillRows.sql
@@ -0,0 +1,7 @@
+INSERT INTO aligned_table1
+    SELECT i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, 'test' || i
+        FROM generate_series(1, 1000000) AS g(i);
+
+INSERT INTO aligned_table2
+    SELECT  'test' || i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i
+        FROM generate_series(1, 1000000) AS g(i);
