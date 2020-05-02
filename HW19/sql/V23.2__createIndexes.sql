a12803cfb89f6a16a471ce36c30ebfa9d516a28c
Edward <op_230@mail.ru>
2020-05-02 14:48:07 +0300

    Hw19 added

--- HW19/sql/V23.2__createIndexes.sql
+++ HW19/sql/V23.2__createIndexes.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_gin_hw19 ON hw19_gin_table USING GIN (to_tsvector('english', content));
-CREATE INDEX idx_gist_hw19 ON hw19_gist_table USING GIST(content);
+CREATE INDEX idx_gist_hw19 ON hw19_gist_table USING GIST(to_tsvector('english', content));
 CREATE INDEX idx_btree_hw19 ON hw19_btree_table(content);
