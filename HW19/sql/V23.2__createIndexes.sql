4d1321c1dc133126d7e1912eff45457def9df840
Edward <op_230@mail.ru>
2020-05-02 15:27:23 +0300

    Added HW19

--- HW19/sql/V23.2__createIndexes.sql
+++ HW19/sql/V23.2__createIndexes.sql
@@ -1,3 +1,3 @@
 CREATE INDEX idx_gin_hw19 ON hw19_gin_table USING GIN (content gin_trgm_ops);
 CREATE INDEX idx_gist_hw19 ON hw19_gist_table USING GIST(content gist_trgm_ops);
-CREATE INDEX idx_btree_hw19 ON hw19_btree_table(content btree_trgm_ops);
+CREATE INDEX idx_btree_hw19 ON hw19_btree_table(content);
