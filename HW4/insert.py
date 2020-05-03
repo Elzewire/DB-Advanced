793055c14e500952f85a8718cd5a07915dc367da
Edward <op_230@mail.ru>
2020-05-03 15:48:23 +0300

    HW4 python insert rule script

---
+++ HW4/insert.py
@@ -0,0 +1,30 @@
+import psycopg2
+import random
+
+#Establishing the connection
+conn = psycopg2.connect(
+   database="postgres", user='postgres', password='postgres', host='localhost', port= '5432'
+)
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+# Preparing SQL queries to INSERT a record into the database.
+for i in range(1, 10001):
+   id = random.randint(1, 10000)
+   data = (id, "test_content%s" % id)
+   if id % 3 == 0:
+      query = "INSERT INTO public.f_table1(id, content) VALUES (%s, %s);"
+      cursor.execute(query, data)
+   elif id % 3 == 1:
+      query = "INSERT INTO public.f_table1(id, content) VALUES (%s, %s);"
+      cursor.execute(query, data)
+   elif id % 3 == 2:
+      query = "INSERT INTO public.f_table1(id, content) VALUES (%s, %s);"
+      cursor.execute(query, data)
+
+# Commit your changes in the database
+conn.commit()
+
+# Closing the connection
+conn.close()
